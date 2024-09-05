eb01e8e71b1c1744286e6f126c77d617e32283ac
puna1175 <myselfpunam.bhuiyan@gmail.com>
2024-09-06 00:00:50 +0600

    product feild added

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Category.css'
+import { Container, Row, Col, Card, Button, Badge, ListGroup } from 'react-bootstrap';
 import { BsCart } from 'react-icons/bs';
-const Category = ({product}) => {
+
+const Category = ({ product }) => {
   return (
-    <div className="product-container">
-    {product.map((product, index) => (
-      <div className="product-card text-start" key={index}>
-        <img src={product.img} alt={product.name} className="product-image" />
-        <h3 className="product-title">{product.name}</h3>
-        <p className="product-price text-start">
-          <span className="new-price">€{product.price}</span>
-          <span className="discount">30% OFF</span>
-        </p>
-        <p className="product-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta saepe laborum incidunt facilis commodi, magnam impedit quidem natus possimus totam rerum tempora. Autem fugit ab eius libero corrupti distinctio perferendis?</p>
-        <button className="cart-button"><BsCart size={24} className="me-2" />Add to cart</button>
+      
+      <div className="product-container">
+        {product.map((product, index) => (
+          <div className="product-card text-start" key={index}>
+            <img src={product.img} alt={product.name} className="product-image" />
+            <h3 className="product-title">{product.name}</h3>
+            <p className="product-price text-start">
+              <span className="new-price">€{product.price}</span>
+              <span className="discount">30% OFF</span>
+            </p>
+            <p className="product-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta saepe laborum incidunt facilis commodi, magnam impedit quidem natus possimus totam rerum tempora. Autem fugit ab eius libero corrupti distinctio perferendis?</p>
+            <button className="cart-button"><BsCart size={24} className="me-2" />Add to cart</button>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
   );
 };
 
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,19 +1,45 @@
 import React, { useEffect, useState } from 'react';
+import './Products.css'
 import Category from '../Category/Category';
-
+import { ListGroup } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Categories = () => {
   const [products, setProducts] = useState([])
-  useEffect(()=>{
+  const [activeItem, setActiveItem] = useState();
+  useEffect(() => {
     fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
-    .then(res=> res.json())
-    .then(data => setProducts(data))
-  },[])
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, [])
+  
+
+  const handleItemClick = (item) => {
+    setActiveItem(item);
+  };
   return (
-    <div>
-      <Category product={products}></Category>
+    <div className="row">
+      <div className="col-md-2 col-md-sm-6">
+      <ListGroup className='pt-5 ms-5'>
+            <ListGroup.Item style={{ backgroundColor: activeItem === 'rocking' ? 'black' : 'white'  , color: activeItem === 'rocking' ? 'white' : 'black', }}
+              onClick={() => handleItemClick('rocking')} action >
+              Rocking Chair
+            </ListGroup.Item>
+            <ListGroup.Item style={{ backgroundColor: activeItem === 'side' ? 'black' : 'white', color: activeItem === 'side' ? 'white' : 'black', }}
+              onClick={() => handleItemClick('side')} action >
+              Side Chair
+            </ListGroup.Item>
+            <ListGroup.Item style={{ backgroundColor: activeItem === 'lounge' ? 'black' : 'white', color: activeItem === 'lounge' ? 'white' : 'black', }}
+              onClick={() => handleItemClick('lounge')} >
+              Lounge Chair
+            </ListGroup.Item>
+          </ListGroup>
+      </div>
+      <div className="col-md-10 col-md-sm-6">
+        <Category product={products}></Category>
+      </div>
     </div>
-  );
+  )
 };
 
 export default Categories;
